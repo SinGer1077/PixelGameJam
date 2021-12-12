88f4fc48c183538c22ec24a119f1c113bfec5fac
GrayStr <agafoner@gmail.com>
2021-12-12 07:30:01 +0500

    Поправка отключения канвас

--- CoronaCop/Assets/MultiplierText.cs
+++ CoronaCop/Assets/MultiplierText.cs
@@ -6,15 +6,27 @@ using UnityEngine.UI;
 public class MultiplierText : MonoBehaviour
 {
     private CharacterMover playerMover;
+    private LevelCore core;
+    private float timer1=1;
     // Start is called before the first frame update
     void Start()
     {
         playerMover = FindObjectOfType<CharacterMover>();
+        core = FindObjectOfType<LevelCore>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer1+=Time.deltaTime;
+        if (timer1 > 1f)
+        {
+            timer1 = 0;
+            if (core.running)
+            {
+                GetComponent<Canvas>().enabled = true;
+            } else GetComponent<Canvas>().enabled = false;
+        }
         /*if (playerMover.i)
         GetComponent<Text>().text=*/
     }
